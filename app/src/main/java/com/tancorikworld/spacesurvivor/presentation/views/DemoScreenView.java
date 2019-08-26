27a0b4e85884ad1962fc279e4e93053e9c28c7fa
Tancorik <wowtancorik@gmail.com>
2019-08-26 07:22:40 +0300

    TASK-1 добавлена простейшая фигура без текстуры

--- app/src/main/java/com/tancorikworld/spacesurvivor/presentation/views/DemoScreenView.java
+++ app/src/main/java/com/tancorikworld/spacesurvivor/presentation/views/DemoScreenView.java
@@ -1,6 +1,7 @@
 package com.tancorikworld.spacesurvivor.presentation.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -21,14 +22,20 @@ public class DemoScreenView extends GLSurfaceView {
         mRenderer = new DemoRenderer();
 
         setRenderer(mRenderer);
+
+        // установить режим отрисовки по запросу
         // setRenderMode(RENDERMODE_WHEN_DIRTY);
+        // requestRender(); // запрос перерисовки
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         mRenderer.setCoord(event.getX(), event.getY());
-
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
